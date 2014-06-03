9fb995986f662de547e32211a1f74558b27f10a9
MicroGit1 <mejdys@microstrategy.com>
2014-06-03 15:15:06 +0200

    FirstCommit

---
+++ src/org/mejdys/spring/MainController.java
@@ -0,0 +1,32 @@
+package org.mejdys.spring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.mejdys.spring.User;
+ 
+@Controller
+@RequestMapping(value = "/")
+public class MainController {
+    
+    @RequestMapping(value = "/")
+    public String home(Model model) {
+    	User user = new User();
+        model.addAttribute("user", user);
+        return "login";
+    }
+    
+    @RequestMapping(value = "/handleLogin", method = RequestMethod.POST)
+    public String handleLogin(@ModelAttribute("user") User user, Model model) {
+        String message = "";
+        if (user.getPassword().equals("letmein")) {
+            message = "Welcome " + user.getLogin() + " !";
+        } else {
+            message = "You are unauthorized to access!";
+        }
+        model.addAttribute("message", message);
+        return "result";
+    }
+}
